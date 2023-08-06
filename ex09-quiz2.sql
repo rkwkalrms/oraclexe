0fc04c45429404f846fb94ef5178f8bf3ddd21f1
rkwkalrms <calen6248@gmail.com>
2023-08-06 17:53:28 +0900

    Create ex09-quiz2.sql

---
+++ ex09-quiz2.sql
@@ -0,0 +1,17 @@
+-- 1. 각 직원의 성(last_name)과 해당 직원의 매니저인 직원의 성(last_name) 조회하기
+-- 2. 각 직원의 성(last_name)과 해당 직원의 부서 이름(department_name) 조회하기
+-- 3. 각 부서의 이름(department_name)과 해당 부서의 평균 급여(avg_salary) 조회하기
+-- 4. 각 부서의 이름(department_name)과 해당 부서의 최대 급여(max_salary) 조회하기
+-- 5. 각 직원의 성(last_name)과 해당 직원이 속한 부서의 최소 급여(min_salary) 조회하기
+-- 6. 각 부서의 이름(department_name)과 해당 부서에 속한 직원 중 가장 높은 급여(highest_salary) 조회하기
+-- 7. 각 직원의 성(last_name)과 해당 직원의 매니저의 성(last_name) 및 부서 이름(department_name) 조회하기
+-- 8. 각 직원의 성(last_name)과 해당 직원이 속한 부서의 매니저의 성(last_name) 조회하기
+-- 9. 각 직원의 성(last_name)과 해당 직원의 보고 상사가 있는 경우 보고 상사의 성(last_name) 조회하기
+-- 10. 직원들 중에서 급여(salary)가 10000 이상인 직원들의 성(last_name)과 해당 직원의 부서 이름(department_name) 조회하기
+/*
+11.
+각 부서의 이름(department_name), 해당 부서의 매니저의 ID(manager_id)와 매니저의 성(last_name),
+직원의 ID(employee_id), 직원의 성(last_name), 그리고 해당 직원의 급여(salary) 조회하기.
+직원들의 급여(salary)가 해당 부서의 평균 급여보다 높은 직원들을 조회합니다.
+결과는 부서 이름과 직원의 급여가 높은 순으로 정렬됩니다.
+*/
